8790b07f5038750c9544f710d54e4c54db0efd4f
Krzysztof Bętkowski <krzysztof.betkowski00@gmail.com>
2022-09-29 22:48:22 +0200

    ObjectTracker code refactor.

--- src/main/java/pl/edu/pk/mech/ObjectTracker.java
+++ src/main/java/pl/edu/pk/mech/ObjectTracker.java
@@ -6,32 +6,34 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
+
+import static org.opencv.imgproc.Imgproc.*;
 
 public class ObjectTracker {
 
-    private static final Logger LOGGER = Logger.getLogger(ObjectTracker.class.getName());
-
-    private static final OpenCVFrameConverter.ToOrgOpenCvCoreMat converter = new OpenCVFrameConverter.ToOrgOpenCvCoreMat();
+    private static final OpenCVFrameConverter.ToOrgOpenCvCoreMat converter
+            = new OpenCVFrameConverter.ToOrgOpenCvCoreMat();
 
     public Frame track(final Frame frame, final double thresholdVal) {
+        // Converting Frame to Matrix
         final Mat src = converter.convert(frame);
-        //Converting the source image to binary
+
+        // Converting the source image to binary
         final Mat gray = new Mat(src.rows(), src.cols(), src.type());
-        Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+        cvtColor(src, gray, COLOR_BGR2GRAY);
         final Mat binary = new Mat(src.rows(), src.cols(), src.type(), new Scalar(0));
-        Imgproc.threshold(gray, binary, thresholdVal, 255, Imgproc.THRESH_BINARY);
-        //Finding Contours
+        threshold(gray, binary, thresholdVal, 255, THRESH_BINARY);
+
+        // Finding Contours
         final List<MatOfPoint> contours = new ArrayList<>();
         final Mat hierarchy = new Mat();
-        Imgproc.findContours(binary, contours, hierarchy, Imgproc.RETR_TREE,
-                Imgproc.CHAIN_APPROX_SIMPLE);
-        //Drawing the Contours
+        findContours(binary, contours, hierarchy, RETR_TREE,
+                CHAIN_APPROX_SIMPLE);
+        // Drawing the Contours
         final Scalar color = new Scalar(0, 0, 255);
 //        Imgproc.drawContours(src, contours, -1, color, 2, Imgproc.LINE_8,
 //                hierarchy, 2, new Point());
@@ -39,26 +41,26 @@ public class ObjectTracker {
         // Drawing cross in the centers of the contours
         for (final MatOfPoint contour : contours) {
 
-            final Moments moments = Imgproc.moments(contour);
+            final Moments moments = moments(contour);
             final double cX = moments.get_m10() / moments.get_m00();
             final double cY = moments.get_m01() / moments.get_m00();
 
-            //Imgproc.circle(src, new Point(cX, cY), 7, color,-1);
+            //circle(src, new Point(cX, cY), 7, color,-1);
             drawCross(src, cX, cY, color);
 
-            Imgproc.putText(src, "center", new Point(cX - 40, cY - 40),
-                    Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, color, 2);
+            putText(src, "center", new Point(cX - 40, cY - 40),
+                    FONT_HERSHEY_SIMPLEX, 0.5, color, 2);
         }
 
         // Converting image to 3 channels for JavaFX
-        Imgproc.cvtColor(binary, binary, Imgproc.COLOR_GRAY2BGR);
+        cvtColor(binary, binary, COLOR_GRAY2BGR);
 
         return converter.convert(binary);
     }
 
-    private void drawCross(final Mat image, final double cX, final double cY, final Scalar color) {
-        Imgproc.line(image, new Point(cX - 30 / 2., cY), new Point(cX + 30 / 2., cY), color, 2);
-        Imgproc.line(image, new Point(cX, cY - 30 / 2.), new Point(cX, cY + 30 / 2.), color, 2);
+    private static void drawCross(final Mat image, final double cX, final double cY, final Scalar color) {
+        line(image, new Point(cX - 30 / 2., cY), new Point(cX + 30 / 2., cY), color, 2);
+        line(image, new Point(cX, cY - 30 / 2.), new Point(cX, cY + 30 / 2.), color, 2);
     }
 
 }
