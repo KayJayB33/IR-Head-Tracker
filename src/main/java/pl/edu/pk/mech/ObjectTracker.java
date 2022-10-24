1469539d6feb2a04eb2a3943cd447a01ec82d922
Krzysztof Bętkowski <krzysztof.betkowski00@gmail.com>
2022-10-24 21:07:56 +0200

    First solvePnP attempt.

--- src/main/java/pl/edu/pk/mech/ObjectTracker.java
+++ src/main/java/pl/edu/pk/mech/ObjectTracker.java
@@ -2,25 +2,34 @@ package pl.edu.pk.mech;
 
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
-import org.bytedeco.opencv.opencv_core.Point2f;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
 import org.opencv.imgproc.Moments;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static org.opencv.imgproc.Imgproc.*;
 
 public class ObjectTracker {
 
+    private static final float FOV = 56f;
     private static final Scalar RED_COLOR = new Scalar(0, 0, 255);
     private static final OpenCVFrameConverter.ToOrgOpenCvCoreMat converter
             = new OpenCVFrameConverter.ToOrgOpenCvCoreMat();
 
-    private final List<Point2f> points = new ArrayList<>();
+    private final List<Point> points = new ArrayList<>();
+    private final MatOfPoint3f model = new MatOfPoint3f(
+            new Point3(-165, 9, 145), // TOP #1
+            new Point3(0, -80, 0), // LEFT #2
+            new Point3(0, 80, 0), // RIGHT #3
+            new Point3(0, 0, 0) // dummy between #2 and #3
+    );
+
+    private static final Logger LOGGER = Logger.getLogger(ObjectTracker.class.getName());
 
 
     public Frame track(final Frame frame, final float thresholdVal, final float minRadius, final float maxRadius) {
@@ -55,20 +64,20 @@ public class ObjectTracker {
             final float cX = (float) (moments.get_m10() / moments.get_m00());
             final float cY = (float) (moments.get_m01() / moments.get_m00());
 
-            points.add(new Point2f(cX, cY));
+            points.add(new Point(cX, cY));
         }
 
         if(points.size() == 3) {
-            estimateHeadPose();
+            estimateHeadPose(frame);
         }
 
         for (int i = 0; i < points.size(); i++) {
-            final Point2f point = points.get(i);
+            final Point point = points.get(i);
             final float radius = (float) Math.sqrt(contourArea(contours.get(i)) / Math.PI);
-            drawCross(src, point.x(), point.y());
+            drawCross(src, point.x, point.y);
             putText(src,
                     String.format("#%d %.1fpx", i + 1, radius),
-                    new Point(point.x() - 40, point.y() - 40),
+                    new Point(point.x - 40, point.y - 40),
                     FONT_HERSHEY_SIMPLEX,
                     0.8,
                     RED_COLOR,
@@ -87,10 +96,36 @@ public class ObjectTracker {
         line(image, new Point(cX, cY - 30 / 2.), new Point(cX, cY + 30 / 2.), RED_COLOR, 2);
     }
 
-    private void estimateHeadPose() {
+    private void estimateHeadPose(final Frame frame) {
         // Sorting points (assuming there are only 3)
-        points.sort((p1, p2) -> Float.compare(p1.y(), p2.y()));
-        points.subList(1, points.size()).sort((p1, p2) -> Float.compare(p1.x(), p2.x()));
+        points.sort(Comparator.comparingDouble(p -> p.y));
+        points.subList(1, points.size()).sort(Comparator.comparingDouble(p -> p.x));
+
+        final Point dummy2DPoint = new Point(
+                (points.get(1).x + points.get(2).x) / 2,
+                (points.get(1).y + points.get(2).y) / 2
+        );
+
+        final MatOfPoint2f matOfImagePoints = new MatOfPoint2f();
+        matOfImagePoints.fromList(Stream.concat(points.stream(), Stream.of(dummy2DPoint)).toList());
+
+        float fc = (float) (frame.imageWidth / 2. / Math.tan(FOV / 2.));
+        final Mat camMat = cameraMatrix(fc, new Size(frame.imageWidth / 2., frame.imageHeight / 2.));
+        final MatOfDouble coeff = new MatOfDouble(); // dummy
+
+        final Mat rvec = new MatOfFloat();
+        final Mat tvec = new MatOfFloat();
+
+        Calib3d.solvePnP(model, matOfImagePoints, camMat, coeff, rvec, tvec);
+
+        LOGGER.info(String.format("tvec:\n%s", tvec.dump()));
+        LOGGER.info(String.format("rvec:\n%s", rvec.dump()));
     }
 
+    private static Mat cameraMatrix(float f, Size center) {
+        final double[] data = { f, 0, center.width, 0, f, center.height, 0, 0, 1f };
+        Mat m = new Mat(3, 3, CvType.CV_64F);
+        m.put(0, 0, data);
+        return m;
+    }
 }
