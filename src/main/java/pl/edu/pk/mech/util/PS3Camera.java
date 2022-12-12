16c7bcfc6044b61b163cf36b3bc52195c13afbdd
Krzysztof Bętkowski <krzysztof.betkowski00@gmail.com>
2022-12-12 19:04:08 +0100

    Wrapped PS3Eye methods.

--- src/main/java/pl/edu/pk/mech/util/PS3Camera.java
+++ src/main/java/pl/edu/pk/mech/util/PS3Camera.java
@@ -14,14 +14,16 @@ import java.util.stream.Collectors;
 import static org.opencv.core.CvType.CV_8UC3;
 import static pl.edu.pk.mech.tracking.ITracker.MAT_FRAME_CONVERTER;
 
+/**
+ * Helper class for PS3Eye.
+ */
 public class PS3Camera {
-
-    private final PS3Eye ps3eye;
-    private PS3Camera.FrameRate framerate;
 
     private static final Map<String, PS3Eye> devicesMap = Arrays.stream(PS3Eye.getDevices())
             .collect(Collectors.toMap(device -> String.format("PS3 Eye USB%d", device.getUSBPortNumber()),
                     device -> device));
+    private final PS3Eye ps3eye;
+    private PS3Camera.FrameRate framerate;
     private int frameWidth;
     private int frameHeight;
     private byte[] pixels;
@@ -45,6 +47,111 @@ public class PS3Camera {
         PS3Eye.disposeAll();
     }
 
+    public void setGain(final int value) {
+        ps3eye.setGain(value);
+    }
+
+    public int getGain() {
+        return ps3eye.getGain();
+    }
+
+    public void setExposure(final int value) {
+        ps3eye.setExposure(value);
+    }
+
+    public int getExposure() {
+        return ps3eye.getExposure();
+    }
+
+    public void setSharpness(final int value) {
+        ps3eye.setSharpness(value);
+    }
+
+    public int getSharpness() {
+        return ps3eye.getSharpness();
+    }
+
+    public void setHue(final int value) {
+        ps3eye.setHue(value);
+    }
+
+    public int getHue() {
+        return ps3eye.getHue();
+    }
+
+    public void setBrightness(final int value) {
+        ps3eye.setBrightness(value);
+    }
+
+    public int getBrightness() {
+        return ps3eye.getBrightness();
+    }
+
+    public void setContrast(final int value) {
+        ps3eye.setContrast(value);
+    }
+
+    public int getContrast() {
+        return ps3eye.getContrast();
+    }
+
+    public void setRedBalance(final int value) {
+        ps3eye.setRedBalance(value);
+    }
+
+    public int getRedBalance() {
+        return ps3eye.getRedBalance();
+    }
+
+    public void setGreenBalance(final int value) {
+        ps3eye.setGreenBalance(value);
+    }
+
+    public int getGreenBalance() {
+        return ps3eye.getGreenBalance();
+    }
+
+    public void setBlueBalance(final int value) {
+        ps3eye.setBlueBalance(value);
+    }
+
+    public int getBlueBalance() {
+        return ps3eye.getBlueBalance();
+    }
+
+    public void setAutogain(final boolean value) {
+        ps3eye.setAutogain(value);
+    }
+
+    public boolean getAutogain() {
+        return ps3eye.getAutogain();
+    }
+
+    public void setAutoWhiteBalance(final boolean value) {
+        ps3eye.setAutoWhiteBalance(value);
+    }
+
+    public boolean getAutoWhiteBalance() {
+        return ps3eye.getAutoWhiteBalance();
+    }
+
+    public void setFlip(final boolean horizontalFlip, final boolean verticalFlip) {
+        ps3eye.setFlip(horizontalFlip, verticalFlip);
+    }
+
+    public boolean getHorizontalFlip() {
+        return ps3eye.getFlipH();
+    }
+
+    public boolean getVerticalFlip() {
+        return ps3eye.getFlipV();
+    }
+
+
+    public static String[] getDevicesNames() {
+        return devicesMap.keySet().toArray(String[]::new);
+    }
+
     public void start() {
         ps3eye.start();
         frameWidth = ps3eye.getResolution().w;
@@ -65,10 +172,6 @@ public class PS3Camera {
         finalImage.put(0, 0, pixels);
 
         return MAT_FRAME_CONVERTER.convert(finalImage);
-    }
-
-    public static String[] getDevicesNames() {
-        return devicesMap.keySet().toArray(String[]::new);
     }
 
     public double getFrameRate() {
